b1cf704cc100589dc117f0a199c3994ef4f293e1
maxim <maxim218maxim218@yandex.ru>
2018-05-16 00:11:22 +0300

    Add some comments

--- static/modules/network/AjaxWorker.js
+++ static/modules/network/AjaxWorker.js
@@ -3,17 +3,31 @@
 import MessagePrinter from "../render/MessagePrinter";
 import getApplicationMode from "../utils/DebugMode";
 
+// урл для отладки (на локалке)
 const DEBUG_URL = "http://localhost:8081";
-
-/* const RELEASE_URL = "http://funny-race-backend-server-1234.herokuapp.com/"; */
+// урл в интернете
 const RELEASE_URL = "https://tp-sharkteam-backend.herokuapp.com/";
 
+/**
+ * класс для отправки запросов на сервер
+ */
 export default class AjaxWorker {
+    /**
+     * конструктор для инициализации части урла запроса и тела запроса
+     * @param url - часть урла запроса
+     * @param body
+     */
     constructor(url, body) {
+        // инициализация урла
         this.url = AjaxWorker.getBasicUrl() + url;
+        // инициализация теа запроса (если это GET запрос, то null)
         this.body = body;
     }
 
+    /**
+     * получение начала урла (различется для рабочего режима и режима отладки)
+     * @returns {string}
+     */
     static getBasicUrl() {
         if(getApplicationMode()) {
             return DEBUG_URL;
@@ -22,58 +36,61 @@ export default class AjaxWorker {
         }
     }
 
+    /**
+     * метод для отправки запроса (возвращает промис)
+     * @returns {Promise<any>}
+     */
     getPromise() {
+        // возвращаем промис
         return new Promise((resolve, reject) => {
             let xhr = new XMLHttpRequest();
-
+            // если тело существует
             if(this.body) {
                 console.log("POST");
+                // создаём POST запрос
                 xhr.open("POST", this.url, true);
             } else {
                 console.log("GET");
+                // создаём GET запрос
                 xhr.open("GET", this.url, true);
             }
-
+            // разрешаем работу с печеньками
             xhr.withCredentials = true;
-
+            // в зависимости от типа запроса (POST или GET) задаём mime тип
             if(this.body) {
+                // для POST запроса
                 xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
             } else {
+                // для GET запроса
                 xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
             }
-
+            // если это POST запрос
             if(this.body) {
+                // отправляем тело запроса
                 xhr.send(JSON.stringify(this.body));
             } else {
+                // отправляем пустое тело
                 xhr.send(null);
             }
-
+            // при получении ответа от сервера
             xhr.onreadystatechange = () => {
+                // если пришёл последний пакет
                 if (xhr.readyState === 4) {
+                    // если код НЕ ошибочный
                     if (xhr.status < 300) {
-                        const xhrResult = xhr.responseText.toString();
-                        const status = xhr.status;
-                        xhr = null;
-                        const answer = xhrResult.toString();
-                        const message = "Answer: " + answer;
-                        if (message.length < 200) {
-                            MessagePrinter.write(message);
-                        }
-
-                        console.log(xhrResult);
-                        resolve(xhrResult);
+                        resolve(xhr);
                     } else {
-                        reject("error string");
+                        reject(xhr);
                     }
                 }
             };
-
-            xhr.onerror = (err) => {
-                reject({type: 'connection', error: err});
-            }
         });
     }
 
+    /**
+     * метод для отправки запроса и получения промиса
+     * @returns {Promise<any>}
+     */
     sendPost() {
         return this.getPromise();
     }

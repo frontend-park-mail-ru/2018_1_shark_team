2e116242173c4a5180223ff5869ddc34ebb531d7
maxim <maxim218maxim218@yandex.ru>
2018-05-15 13:55:14 +0300

    Change ajax

--- static/modules/network/AjaxWorker.js
+++ static/modules/network/AjaxWorker.js
@@ -4,7 +4,9 @@ import MessagePrinter from "../render/MessagePrinter";
 import getApplicationMode from "../utils/DebugMode";
 
 const DEBUG_URL = "http://localhost:5005/";
-const RELEASE_URL = "http://funny-race-backend-server-1234.herokuapp.com/";
+
+/* const RELEASE_URL = "http://funny-race-backend-server-1234.herokuapp.com/"; */
+const RELEASE_URL = "https://tp-sharkteam-backend.herokuapp.com/";
 
 export default class AjaxWorker {
     constructor(url, body) {
@@ -22,20 +24,23 @@ export default class AjaxWorker {
 
     getPromise() {
         return new Promise((resolve, reject) => {
-            // init xhr properties
             let xhr = new XMLHttpRequest();
-            xhr.open("POST", this.url, true);
+            if(this.body) {
+                xhr.open("POST", this.url, true);
+            } else {
+                xhr.open("GET", this.url, true);
+            }
             xhr.withCredentials = true;
             xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-            try {
+
+            if(this.body) {
                 xhr.send(JSON.stringify(this.body));
-            } catch (err) {
-                reject(err);
+            } else {
+                xhr.send(null);
             }
 
-            // on getting result from server
             xhr.onreadystatechange = () => {
-                if (xhr.readyState === 4 && xhr.status === 200) {
+                if (xhr.readyState === 4) {
                     const xhrResult = xhr.responseText.toString();
                     xhr = null;
                     const answer = xhrResult.toString();
@@ -43,12 +48,14 @@ export default class AjaxWorker {
                     if (message.length < 200) {
                         MessagePrinter.write(message);
                     }
-                    resolve(xhrResult);
-                }
-            };
 
-            xhr.onerror = () => {
-                reject(new TypeError("Network request failed"));
+                    const resObj = {
+                        xhrResult: xhrResult,
+                        status: xhr.status,
+                    };
+
+                    resolve(resObj);
+                }
             };
         });
     }

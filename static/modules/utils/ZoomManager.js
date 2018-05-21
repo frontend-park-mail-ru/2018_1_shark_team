a97f0fd26c5a6c8c6ff0fda73d8f76b605aa478a
maxim <maxim218maxim218@yandex.ru>
2018-05-21 04:44:44 +0300

    Simplify zoom

--- static/modules/utils/ZoomManager.js
+++ static/modules/utils/ZoomManager.js
@@ -1,33 +1,12 @@
 "use strict";
 
-const FULL = 1;
-const MIDDLE = 0.5;
-const WAIT = 2000;
+import LogMessage from "../../gameFiles/scripts/MessageLogger";
+
+const ZOOM_VALUE = 0.5;
 
 export default class ZoomManager {
     constructor() {
-        ZoomManager.resizeAction();
-        this.addResizeEvent();
-
-        let zoomInterval = setInterval(() => {
-            ZoomManager.resizeAction();
-        }, WAIT);
-    }
-
-    static resizeAction() {
-        const styleObj = getComputedStyle(document.body);
-        const heightBody = parseInt(styleObj.height);
-        const heightClient = parseInt(document.documentElement.clientHeight);
-        if(heightBody < heightClient) {
-            document.body.style.zoom = FULL.toString();
-        } else {
-            document.body.style.zoom = MIDDLE.toString();
-        }
-    }
-
-    addResizeEvent() {
-        window.onresize = ()=> {
-            ZoomManager.resizeAction();
-        };
+        document.body.style.zoom = ZOOM_VALUE.toString();
+        LogMessage("Set zoom OK");
     }
 }

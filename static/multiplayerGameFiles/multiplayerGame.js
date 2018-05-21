8a502c8084f984b439eab1f965ad122289c21370
maxim <maxim218maxim218@yandex.ru>
2018-05-21 21:26:21 +0300

    Add starting multiplayergame

---
+++ static/multiplayerGameFiles/multiplayerGame.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import LogMessage from "../gameFiles/scripts/MessageLogger";
+
+export default function multiplayerGame() {
+    LogMessage("Finding enemy");
+}
--- static/views/multiplayer-page/MultiplayerPage.js
+++ static/views/multiplayer-page/MultiplayerPage.js
@@ -1,6 +1,8 @@
 "use strict";
 
 import template from "./multiplayer-page.pug";
+import LogMessage from "../../gameFiles/scripts/MessageLogger";
+import multiplayerGame from "../../multiplayerGameFiles/multiplayerGame";
 
 export default class MultiplayerPage {
     constructor() {
@@ -14,6 +16,11 @@ export default class MultiplayerPage {
     static addEventsToElements() {
         document.querySelector(".multiplayer-page__main-menu-button").onclick = () => {
             window.location = "/main-menu";
+        };
+
+        document.querySelector(".multiplayer-page__start-game-finding-button").onclick = () => {
+            LogMessage("START fining enemy multiplayer");
+            multiplayerGame();
         };
     }
 }

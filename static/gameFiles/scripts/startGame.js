92a1c817c66cc09689747dc1d231720f56bca80a
maxim <maxim218maxim218@yandex.ru>
2018-05-21 16:08:03 +0300

    Game hidden control

--- static/gameFiles/scripts/startGame.js
+++ static/gameFiles/scripts/startGame.js
@@ -3,6 +3,7 @@
 import Game from "./Game";
 
 export default function startGame() {
+    document.querySelector(".one-player-page__canvas-box").hidden = false;
     document.querySelector(".one-player-page__start-game-button").hidden = true;
     document.querySelector(".canvas-box__canvas-plain").style.opacity = 1;
     new Game();

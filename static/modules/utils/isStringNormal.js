eef835392edb372d14bce862cedd1a87e56e36e5
Oleg Morozenkov <m@oleg.rocks>
2018-05-23 12:15:07 +0300

    isStringNormal regex

--- static/modules/utils/isStringNormal.js
+++ static/modules/utils/isStringNormal.js
@@ -6,17 +6,7 @@
  * @returns {boolean}
  */
 export default function isStringNormal(s) {
-    function normalChar(charParam) {
-        const c = charParam.toLowerCase();
-        const allowedChars = "abcdefghijklmnopqrstuvwxyz0123456789";
-        return allowedChars.indexOf(c) !== -1;
-    }
-
-    for(let i = 0; i < s.length; i++) {
-        const c = s.charAt(i);
-        if(normalChar(c) === false) {
-            return false;
-        }
-    }
-    return true;
+    const invalidStringRegex = /[^a-zA-Z0-9]/;
+    const isInvalid = invalidStringRegex.test(s);
+    return !isInvalid;
 }

26d5851de7f1dcb544d58fb5b54406b2270a80af
maxim <maxim218maxim218@yandex.ru>
2018-05-28 01:07:53 +0300

    Liders page main menu link

--- src/views/liders-page/LidersPage.js
+++ src/views/liders-page/LidersPage.js
@@ -2,6 +2,7 @@
 
 import template from "./liders-page.pug";
 import getLiaderBoard from "../../modules/LiderBoard/LiderBoardLoader";
+import LogMessage from "../../gameFiles/scripts/MessageLogger";
 
 export default class LidersPage {
     constructor() {
@@ -13,9 +14,7 @@ export default class LidersPage {
     }
 
     static addEventsToElements(router) {
-        document.querySelector(".liders-page__main-menu-button").addEventListener("click", () => {
-            router.moveToPage("/main-menu");
-        });
+        LogMessage("Router: " + router);
 
         document.querySelector(".liders-page__previous-list-button").addEventListener("click", () => {
             const liaderBoard = getLiaderBoard();

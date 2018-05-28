45f439d88e426c611045fc9e5ae816e59ba41e9e
maxim <maxim218maxim218@yandex.ru>
2018-05-29 02:20:57 +0300

    Liders page with places and avatars

--- src/modules/LiderBoard/render/LiderBoardRender.js
+++ src/modules/LiderBoard/render/LiderBoardRender.js
@@ -10,13 +10,17 @@ export default class LiderBoardRender {
     }
 
     writeArrContent(arr) {
+        let tableContent = "<table>";
         arr.forEach((element) => {
-            const content = element.login + " : " + element.score;
-            const div = document.createElement("div");
-            const text = document.createTextNode(content);
-            div.appendChild(text);
-            this.elementsBase.getElement("lidersBox").appendChild(div);
+            let image = element.imagePlayer;
+            if(image === "IMAGE_NOT_SET") {
+                image = "./images/userImage.jpg";
+            }
+            const content = "<tr><td>" +  element.playerPlace.toString() + "</td><td>" + "<img width = '50px' height = '50px' src = '" + image + "'>" + "</td><td>" + element.login + "</td><td>" + element.score + "</td></tr>";
+            tableContent += content.toString();
         });
+        tableContent += "</table>";
+        this.elementsBase.getElement("lidersBox").innerHTML = tableContent;
     }
 
     writeEmptyListMessage() {

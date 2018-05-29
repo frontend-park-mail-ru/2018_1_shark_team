05c9f4600c91af85c3c3a4470786372bbb0f7ed4
maxim <maxim218maxim218@yandex.ru>
2018-05-29 05:04:09 +0300

    Add touch class for events multiplay

---
+++ src/multiplayerGameFiles/TouchEventsController.js
@@ -0,0 +1,34 @@
+"use strict";
+
+import LogMessage from "../gameFiles/scripts/MessageLogger";
+
+export default class TouchEventsController {
+    static getCanvasElement() {
+        LogMessage("Find canvas for two players");
+        return document.querySelector(".canvas-multiplayer-box__canvas-plain");
+    }
+
+    static addTouchEvents(touchStartFunctionCallback, touchEndFunctionCallback) {
+        const canvasObj = TouchEventsController.getCanvasElement();
+        /////////////////////////////////////////////////////
+        canvasObj.ontouchstart = (event) => {
+            touchStartFunctionCallback(event);
+        };
+        /////////////////////////////////////////////////////
+        canvasObj.ontouchend = () => {
+            touchEndFunctionCallback();
+        };
+    }
+
+    static dropTouchEvents() {
+        const canvasObj = TouchEventsController.getCanvasElement();
+        /////////////////////////////////////////////////////
+        canvasObj.ontouchstart = () => {
+            LogMessage("Canvas TOUCH START Killed");
+        };
+        /////////////////////////////////////////////////////
+        canvasObj.ontouchend = () => {
+            LogMessage("Canvas TOUCH END Killed");
+        };
+    }
+}
